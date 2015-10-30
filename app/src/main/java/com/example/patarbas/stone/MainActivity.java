c074656b17ac39d0162388f7ac0401dbb9f3c394
mukondono <nathanielreline@gmail.com>
2015-10-30 10:48:15 -0700

    Added toolbar

--- app/src/main/java/com/example/patarbas/stone/MainActivity.java
+++ app/src/main/java/com/example/patarbas/stone/MainActivity.java
@@ -4,18 +4,22 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
     @Override

2e8e065529c6392d830073b8ea609477158317ee
Patrick Arbasak <patarbas@uat.edu>
2015-11-12 21:55:13 -0700

    Added some task functionality

--- app/src/main/java/com/example/patarbas/stone/MainActivity.java
+++ app/src/main/java/com/example/patarbas/stone/MainActivity.java
@@ -1,33 +1,86 @@
 package com.example.patarbas.stone;
 
-import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.ListActivity;
+import android.content.ContentValues;
 import android.content.DialogInterface;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.database.sqlite.SQLiteDatabase;
+import android.widget.SimpleCursorAdapter;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.patarbas.stone.db.TaskContract;
+import com.example.patarbas.stone.db.TaskDBHelper;
+
+public class MainActivity extends ListActivity {
+
+    private TaskDBHelper helper;
+
+    private SimpleCursorAdapter listAdapter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        (findViewById(R.id.btnAddTask)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        Log.e("Exception", "OMG I RAN");
+        SQLiteDatabase sqlDB = new TaskDBHelper(this).getWritableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns.TASK},
+                null,null,null,null,null);
+
+        cursor.moveToFirst();
+        while(cursor.moveToNext()) {
+            Log.d("MainActivity cursor",
+                    cursor.getString(
+                            cursor.getColumnIndexOrThrow(
+                                    TaskContract.Columns.TASK)));
+        }
+        updateUI();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        Log.e("Exception", "OMG I RAN");
-        getMenuInflater().inflate(R.menu.menu,menu);
+        getMenuInflater().inflate(R.menu.menu, menu);
         return true;
+    }
+
+    private void updateUI() {
+        helper = new TaskDBHelper(MainActivity.this);
+        SQLiteDatabase sqlDB = helper.getReadableDatabase();
+        Cursor cursor = sqlDB.query(TaskContract.TABLE,
+                new String[]{TaskContract.Columns._ID, TaskContract.Columns.TASK},
+                null, null, null, null, null);
+
+        listAdapter = new SimpleCursorAdapter(
+                this,
+                R.layout.task_view,
+                cursor,
+                new String[]{TaskContract.Columns.TASK},
+                new int[]{R.id.taskTextView},
+                0
+        );
+        this.setListAdapter(listAdapter);
     }
 
     @Override
@@ -39,18 +92,29 @@ public class MainActivity extends AppCompatActivity {
                 builder.setMessage("What do you want to do?");
                 final EditText inputField = new EditText(this);
                 builder.setView(inputField);
-                builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Log.d("MainActivity",inputField.getText().toString());
-                    }
-                });
+                builder.setPositiveButton("Add",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                String task = inputField.getText().toString();
+                                Log.d("MainActivity", task);
 
+                                helper = new TaskDBHelper(MainActivity.this);
+                                SQLiteDatabase db = helper.getWritableDatabase();
+                                ContentValues values = new ContentValues();
+
+                                values.clear();
+                                values.put(TaskContract.Columns.TASK, task);
+
+                                db.insertWithOnConflict(TaskContract.TABLE, null, values,
+                                        SQLiteDatabase.CONFLICT_IGNORE);
+
+                            }
+                        });
+                updateUI();
                 builder.setNegativeButton("Cancel",null);
-
                 builder.create().show();
                 return true;
-
             default:
                 return false;
         }
--- app/src/main/java/com/example/patarbas/stone/db/TaskContract.java
+++ app/src/main/java/com/example/patarbas/stone/db/TaskContract.java
@@ -1,4 +1,14 @@
 package com.example.patarbas.stone.db;
 
+import android.provider.BaseColumns;
+
 public class TaskContract {
+    public static final String DB_NAME = "com.example.TodoList.db.tasks";
+    public static final int DB_VERSION = 1;
+    public static final String TABLE = "tasks";
+
+    public class Columns {
+        public static final String TASK = "task";
+        public static final String _ID = BaseColumns._ID;
+    }
 }
